9a1871a0d37c4d79f732b1570b80b10512b2ee1c
Mayte Espi Hermida <maytehermida@gmail.com>
2019-10-08 23:04:57 +0200

    added: test coverage

--- mall-builder/mall-service/src/test/java/com/rumbo/mall/service/sale/SaleTaxeServiceTest.java
+++ mall-builder/mall-service/src/test/java/com/rumbo/mall/service/sale/SaleTaxeServiceTest.java
@@ -1,5 +1,6 @@
 package com.rumbo.mall.service.sale;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -12,15 +13,24 @@ import com.rumbo.mall.dto.sale.ProductDTO;
 @ExtendWith( MockitoExtension.class )
 public class SaleTaxeServiceTest {
 
+	private static final String CHOCOLATE_BAR_FOOD_MOCK = "chocolate bar";
 	@InjectMocks
 	private SaleTaxeServiceImpl saleTaxeService;
 	
 	@Test
-	public void findTaxeFood_shouldReturn0() {
+	public void findTaxeFood_shouldReturnProductPrice() {
 		final double productPrice = 12.6454;
-		final double totalPrice = saleTaxeService.findTotalPriceWithTaxe(mockProduct("chocolate bar",productPrice));
+		final double totalPrice = saleTaxeService.findTotalPriceWithTaxe(mockProduct(CHOCOLATE_BAR_FOOD_MOCK,productPrice));
 		assertTrue(totalPrice == productPrice);
 	}
+	
+	@Test
+	public void findTaxeImportedFood_shouldReturnProductPriceIncreased() {
+		final double productPrice = 12.6454;
+		final double totalPrice = saleTaxeService.findTotalPriceWithTaxe(mockProduct("imported" + CHOCOLATE_BAR_FOOD_MOCK,productPrice));
+		assertFalse(totalPrice == productPrice);
+	}
+
 
 	private ProductDTO mockProduct(final String productType, final double price) {
 		return new ProductDTO(productType, price);
